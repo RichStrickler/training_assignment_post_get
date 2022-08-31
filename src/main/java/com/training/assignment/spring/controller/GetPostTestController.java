6682fb351d7ba075a4f244f93cc77d4d55732e8a
Richard Strickler <richastrick1998@gmail.com>
2022-08-31 17:37:48 -0500

    PathVariable

--- src/main/java/com/training/assignment/spring/controller/GetPostTestController.java
+++ src/main/java/com/training/assignment/spring/controller/GetPostTestController.java
@@ -3,6 +3,7 @@ package com.training.assignment.spring.controller;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,7 +26,22 @@ public class GetPostTestController {
   }
 
   @GetMapping("/getTest")
+  @ResponseBody
   public EmployeeData getTest(@RequestParam String lastName) {
+    EmployeeData empData = new EmployeeData();
+
+    empData.setFirstName("FirstName");
+    empData.setLastName(lastName);
+    empData.setDeptId(0);
+    empData.setId(0);
+    empData.setStatCd(0);
+
+    return empData;
+  }
+
+  @GetMapping("/getTest/{lastName}")
+  @ResponseBody
+  public EmployeeData getTest1(@PathVariable("lastName") String lastName) {
     EmployeeData empData = new EmployeeData();
 
     empData.setFirstName("FirstName");
